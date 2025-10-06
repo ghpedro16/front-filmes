0e52ba896dad43f84f95493e431e43efe11b2ed4
PEDRO HENRIQUE ARAUJO DA SILVA <25132515@CFP127ALUNO.LOCAL>
2025-10-06 11:19:19 -0300

    em andamento

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 'use strict'
 
-async function buscarPopulares(){
+const apikey = 15801986
+
+async function carrossel(){
+    const cards = document.createElement('div')
+
+    
+}
+
+async function buscar(){
     const nome = document.getElementById('input').value
-    const apikey = 15801986
     const url = `http://www.omdbapi.com/?apikey=${apikey}&t=${nome}`
 
     const response = await fetch(url)
     const imagens = await response.json()
 
-    const cards = document.getElementById('cards-populares')
+    const cards = document.getElementById('busca')
     cards.innerHTML = ''
 
-    imagens.forEach(urlImagem => {
-        const imagem = document.createElement('img')
-        imagem.src = urlImagem
-        cards.appendChild(imagem)
+    imagens.forEach(function(item){
+        if(nome == item.title){
+            let img = item.poster 
+            img = document.createElement('img')
+            cards.appendChild(img)
+        }
+
     });
 }
 
+const buscar = document.getElementById('btn-pesquisa')
+buscar.addEventListener('click', buscar())
